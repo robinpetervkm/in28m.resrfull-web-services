4db858f3384545cf6c261268d6ff4392bd0d7648
Robin Peter <robin.peter.vkm@gmail.com>
2021-02-28 13:19:36 -0400

    User Class Added

---
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/User.java
@@ -0,0 +1,65 @@
+package com.in28minutes.rest.webservices.restfullwebservices;
+
+import java.util.Date;
+
+public class User {
+	
+	private Integer id;
+	
+	private String name;
+	
+	private Date brithDate;
+
+	public User(Integer id, String name, Date brithDate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.brithDate = brithDate;
+	}
+
+	
+	
+	public Integer getId() {
+		return id;
+	}
+
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public Date getBrithDate() {
+		return brithDate;
+	}
+
+
+
+	public void setBrithDate(Date brithDate) {
+		this.brithDate = brithDate;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", brithDate=" + brithDate + "]";
+	}
+	
+	
+
+}
