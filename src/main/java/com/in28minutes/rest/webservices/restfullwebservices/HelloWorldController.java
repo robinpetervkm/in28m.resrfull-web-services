d04b7aea768687bfa2ddd6929a0d9bd575341324
Robin Peter <robin.peter.vkm@gmail.com>
2021-02-28 11:14:54 -0400

    Rfactor with GetMapping

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/HelloWorldController.java
@@ -1,7 +1,6 @@
 package com.in28minutes.rest.webservices.restfullwebservices;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //controller
@@ -11,7 +10,7 @@ public class HelloWorldController {
 	//GET
 	//URI - //hello-world
 	//method "HelloWorld"
-	@RequestMapping(method = RequestMethod.GET, path = "hello-world")
+	@GetMapping( path = "hello-world")
 	public String helloWorld() {
 		return "Hello World";
 	}
