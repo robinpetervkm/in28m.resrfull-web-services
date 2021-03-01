12a9639dcb706b8337cdd6f0233066ab32f9910c
Robin Peter <robin.peter.vkm@gmail.com>
2021-03-01 15:49:39 -0400

    retrieveAllUsers implemented

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/user/UserResource.java
@@ -1,13 +1,23 @@
 package com.in28minutes.rest.webservices.restfullwebservices.user;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserResource {
 	
+	@Autowired
+	private UserDaoService service;
+	
 	//GET /users
 	//retrieveAllUsers
-
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers(){
+		return service.findAll();
+	}
 	
 	//GET /users/{id}
 	//retrieveUser(int id)
