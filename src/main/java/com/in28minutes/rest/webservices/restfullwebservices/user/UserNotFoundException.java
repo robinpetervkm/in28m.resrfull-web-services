4a31b720ebb831223738e7131be30e714457aaa5
Robin Peter <robin.peter.vkm@gmail.com>
2021-03-01 19:06:32 -0400

    exception handling

---
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/user/UserNotFoundException.java
@@ -0,0 +1,10 @@
+package com.in28minutes.rest.webservices.restfullwebservices.user;
+
+public class UserNotFoundException extends RuntimeException {
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+	
+
+}
--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/user/UserResource.java
@@ -29,7 +29,11 @@ public class UserResource {
 	//retrieveUser(int id)
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
-		return service.findOne(id);
+		User user = service.findOne(id);
+		if(user==null)
+			throw new UserNotFoundException("id-"+id);
+		
+		return user;
 	}
 	
 	//input: details of user
