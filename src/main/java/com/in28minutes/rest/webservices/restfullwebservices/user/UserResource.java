a86c327c2373a8e66e58a6a3110657b00b1d99a6
Robin Peter <robin.peter.vkm@gmail.com>
2021-03-01 16:36:02 -0400

    Save method implemented but getting error because of lake of Default Constructor

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/user/UserResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,6 +29,13 @@ public class UserResource {
 		return service.findOne(id);
 	}
 	
+	//input: details of user
+	
+	//output:	CREATED & Return the created URI
+	@PostMapping("/users")
+	public void createUser(@RequestBody User user) {
+		User savedUser = service.save(user);		
+	}
 	
 
 }
