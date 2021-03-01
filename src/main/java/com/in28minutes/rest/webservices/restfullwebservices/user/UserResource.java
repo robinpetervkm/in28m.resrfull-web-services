5492767a707c2350f99a1dfaef73e8e2e9e15e9f
Robin Peter <robin.peter.vkm@gmail.com>
2021-03-01 15:45:07 -0400

    UserResource--RestController--step one

---
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/user/UserResource.java
@@ -0,0 +1,15 @@
+package com.in28minutes.rest.webservices.restfullwebservices.user;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+	
+	//GET /users
+	//retrieveAllUsers
+
+	
+	//GET /users/{id}
+	//retrieveUser(int id)
+
+}
