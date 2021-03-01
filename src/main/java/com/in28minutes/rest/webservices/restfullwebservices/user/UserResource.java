215dea1d8701b08961ecc3e62d1a805a5b372f07
Robin Peter <robin.peter.vkm@gmail.com>
2021-03-01 16:05:20 -0400

    one user retrieve working

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/user/UserResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,7 +22,8 @@ public class UserResource {
 	
 	//GET /users/{id}
 	//retrieveUser(int id)
-	public User retrieveUser(int id) {
+	@GetMapping("/users/{id}")
+	public User retrieveUser(@PathVariable int id) {
 		return service.findOne(id);
 	}
 	
