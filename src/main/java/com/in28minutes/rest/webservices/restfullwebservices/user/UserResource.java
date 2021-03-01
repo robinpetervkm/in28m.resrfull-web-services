2a8ceb74112b5d88617a03c943adbf6a9f2267e6
Robin Peter <robin.peter.vkm@gmail.com>
2021-03-01 18:47:08 -0400

    record-created-and-link send back

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/user/UserResource.java
@@ -1,13 +1,16 @@
 package com.in28minutes.rest.webservices.restfullwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -33,8 +36,17 @@ public class UserResource {
 	
 	//output:	CREATED & Return the created URI
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
-		User savedUser = service.save(user);		
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
+		User savedUser = service.save(user);	
+		//CREATED
+		// /users/4
+		
+		URI location = ServletUriComponentsBuilder
+		.fromCurrentRequest()
+		.path("/{id}")
+		.buildAndExpand(savedUser.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
 	}
 	
 
