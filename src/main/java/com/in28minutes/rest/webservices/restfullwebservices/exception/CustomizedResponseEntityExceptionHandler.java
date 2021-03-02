4fa33ec57ec1f9248a9a0b469dc6ef18ba46d0b7
Robin Peter <robin.peter.vkm@gmail.com>
2021-03-02 17:22:15 -0400

    lost of info to end user upon validation failed senario

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,5 +35,12 @@ ResponseEntityExceptionHandler  {
 				new ExceptionResponse(new Date(), ex.getMessage(), 
 						request.getDescription(false));
 		return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		ExceptionResponse exceptionResponse = 
+				new ExceptionResponse(new Date(),ex.getMessage() ,ex.getBindingResult().toString());
+		return new ResponseEntity(exceptionResponse,HttpStatus.BAD_REQUEST);
 	}
 }
