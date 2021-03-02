66dde13dca9fb737b528f68fc4da50d96e822bb1
Robin Peter <robin.peter.vkm@gmail.com>
2021-03-01 20:53:14 -0400

    Delete Method

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/user/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,6 +38,14 @@ public class UserResource {
 			throw new UserNotFoundException("id-"+id);
 		
 		return user;
+	}
+	//DELETE /users/{id}
+	//retrieveUser(int id)
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		User user = service.deleteById(id);
+		if(user==null)
+			throw new UserNotFoundException("id-"+id);
 	}
 	
 	//input: details of user
