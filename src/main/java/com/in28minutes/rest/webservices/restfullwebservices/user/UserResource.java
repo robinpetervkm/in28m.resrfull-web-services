f8606190c8b9efad6ec181d5578e95583a6bb140
Robin Peter <robin.peter.vkm@gmail.com>
2021-03-02 09:57:34 -0400

    Validation added--with 400--Bad Request

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/user/UserResource.java
@@ -3,6 +3,8 @@ package com.in28minutes.rest.webservices.restfullwebservices.user;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -52,7 +54,7 @@ public class UserResource {
 	
 	//output:	CREATED & Return the created URI
 	@PostMapping("/users")
-	public ResponseEntity<Object> createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
 		User savedUser = service.save(user);	
 		//CREATED
 		// /users/4
