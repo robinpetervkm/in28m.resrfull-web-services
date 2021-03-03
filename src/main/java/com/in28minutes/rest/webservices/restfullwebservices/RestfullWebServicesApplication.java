058aa4798891b65c1f6d685a8a53e2f0b7c15dc8
Robin Peter <robin.peter.vkm@gmail.com>
2021-03-03 08:26:13 -0400

    all steps completed
    we have error
    
    because mismatch in Basename of message source

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/RestfullWebServicesApplication.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/RestfullWebServicesApplication.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
@@ -21,8 +22,16 @@ public class RestfullWebServicesApplication {
 	@Bean
 	public LocaleResolver localeResolver() {
 		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		//		-Default Locale - Locale.US
 		localeResolver.setDefaultLocale(Locale.US);
 		return localeResolver;
+	}
+	//--ResourceBundleMessageSource
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
 	}
 	
 }
--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/helloworld/HelloWorldController.java
@@ -1,12 +1,22 @@
 package com.in28minutes.rest.webservices.restfullwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 //controller
 @RestController
 public class HelloWorldController {
+	
+	//-- AutoWire Message Source
+	
+	@Autowired
+	private MessageSource messageSource;
 	
 	//GET
 	//URI - //hello-world
@@ -53,8 +63,11 @@ public class HelloWorldController {
 	//URI - //hello-world-internationalized
 	//method "Good Morning"
 	@GetMapping( path = "hello-world-internationalized")
-	public String helloWorldInternationalized() {
-		return "Good Morning";
+	//-- @RequestHeader(value = "Accept-Language",required = false) Locale locale
+	public String helloWorldInternationalized(
+			@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+		//-- messageSource.getMessage("helloWorld.message",null,locale)
+		return messageSource.getMessage("good.morning.message",null,locale);
 	}
 
 }
