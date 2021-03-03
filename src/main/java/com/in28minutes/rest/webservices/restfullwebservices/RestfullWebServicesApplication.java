b09902359e0e5f76e22b2c99cf9e2f57bdab7ec8
Robin Peter <robin.peter.vkm@gmail.com>
2021-03-03 07:58:08 -0400

    steps
    configuration part done

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/RestfullWebServicesApplication.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/RestfullWebServicesApplication.java
@@ -1,7 +1,12 @@
 package com.in28minutes.rest.webservices.restfullwebservices;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class RestfullWebServicesApplication {
@@ -9,5 +14,15 @@ public class RestfullWebServicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestfullWebServicesApplication.class, args);
 	}
-
+	//###Configuration
+	
+	//--LocaleResolver
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+	
 }
--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/helloworld/HelloWorldController.java
@@ -29,5 +29,32 @@ public class HelloWorldController {
 	public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
+	
+	
+	//###Internationalization
+	
+	//###Configuration
+	
+	//--LocaleResolver
+	//	-Default Locale - Locale.US
+	//--ResourceBundleMessageSource
+	
+	
+	//###Usage
+	//-- AutoWire Message Source
+	//-- @RequestHeader(value = "Accept-Language",required = false) Locale locale
+	//-- messageSource.getMessage("helloWorld.message",null,locale)
+	
+	
+	
+	
+	
+	//GET
+	//URI - //hello-world-internationalized
+	//method "Good Morning"
+	@GetMapping( path = "hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		return "Good Morning";
+	}
 
 }
