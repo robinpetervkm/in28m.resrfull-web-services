9f3b808cac52321d9d7bc03371e266faee33abf1
Robin Peter <robin.peter.vkm@gmail.com>
2021-03-05 19:12:17 -0400

    localhost:8080/v2/api-docs

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/SwaggerConfig.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/SwaggerConfig.java
@@ -1,9 +1,20 @@
 package com.in28minutes.rest.webservices.restfullwebservices;
 
-//Configuration
-//Enable Swagger
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 	
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2);
+	}
 	// Bean - Docket
 		//Swagger 2
 		//All the paths
