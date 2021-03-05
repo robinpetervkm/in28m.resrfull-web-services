6403c9204deba64d38500cd217362bf1dfde0cba
Robin Peter <robin.peter.vkm@gmail.com>
2021-03-05 15:37:16 -0400

    Spring provide LocaleContextHolder.

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/RestfullWebServicesApplication.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/RestfullWebServicesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
@@ -21,7 +22,7 @@ public class RestfullWebServicesApplication {
 	
 	@Bean
 	public LocaleResolver localeResolver() {
-		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
 		//		-Default Locale - Locale.US
 		localeResolver.setDefaultLocale(Locale.US);
 		return localeResolver;
--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/helloworld/HelloWorldController.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -64,10 +65,11 @@ public class HelloWorldController {
 	//method "Good Morning"
 	@GetMapping( path = "hello-world-internationalized")
 	//-- @RequestHeader(value = "Accept-Language",required = false) Locale locale
-	public String helloWorldInternationalized(
-			@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+	public String helloWorldInternationalized() {
 		//-- messageSource.getMessage("helloWorld.message",null,locale)
-		return messageSource.getMessage("good.morning.message",null,locale);
+		
+		//Spring- Provide LocaleConextHolder---
+		return messageSource.getMessage("good.morning.message",null,LocaleContextHolder.getLocale());
 	}
 
 }
