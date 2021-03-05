6007c55f0ecff8ce08c7aacdfbb4ff36e6cf4450
Robin Peter <robin.peter.vkm@gmail.com>
2021-03-05 17:23:58 -0400

    Swagger Steps to configuration

---
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/SwaggerConfig.java
@@ -0,0 +1,11 @@
+package com.in28minutes.rest.webservices.restfullwebservices;
+
+//Configuration
+//Enable Swagger
+public class SwaggerConfig {
+	
+	// Bean - Docket
+		//Swagger 2
+		//All the paths
+		//All the Apis
+}	
