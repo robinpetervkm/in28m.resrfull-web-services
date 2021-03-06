d6e5a88cee681d5f49169f93e545c13f1faf92d5
Robin Peter <robin.peter.vkm@gmail.com>
2021-03-05 22:24:41 -0400

    produces consumes

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/SwaggerConfig.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/SwaggerConfig.java
@@ -1,6 +1,9 @@
 package com.in28minutes.rest.webservices.restfullwebservices;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,11 +32,16 @@ public static final ApiInfo DEFAULT_API_INFO
     "Awesome Apache 2.0",
     "http://www.apache.org/licenses/LICENSE-2.0",
     new ArrayList<>());
+private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<String>(
+		Arrays.asList("application/json","application/xml"));
 
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
-				.apiInfo(DEFAULT_API_INFO);
+				.apiInfo(DEFAULT_API_INFO)
+				.produces(DEFAULT_PRODUCES_AND_CONSUMES)
+				.consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+
 	}
 	// Bean - Docket
 		//Swagger 2
