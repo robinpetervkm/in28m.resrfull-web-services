a09cfcaf1d9a643786b9302810e5156a862017a9
Robin Peter <robin.peter.vkm@gmail.com>
2021-03-09 15:58:31 -0400

    Filtering-list is implemented

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/filter/FilteringController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/filter/FilteringController.java
@@ -1,5 +1,8 @@
 package com.in28minutes.rest.webservices.restfullwebservices.filter;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +11,10 @@ public class FilteringController {
 	@GetMapping("/filtering")
 	public SomeBean retriveSomeBean() {
 		return new SomeBean("value1","value2","value3");
+	}
+	@GetMapping("/filtering-list")
+	public List<SomeBean> retriveListOfSomeBean() {
+		return Arrays.asList(new SomeBean("value1","value2","value3"),
+				new SomeBean("value11","value22","value33"));
 	}
 }
