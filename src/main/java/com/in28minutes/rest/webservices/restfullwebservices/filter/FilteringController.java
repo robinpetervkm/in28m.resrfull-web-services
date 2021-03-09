4533a57d23cc6fca3a5349a5e8208ba62b7c2caa
Robin Peter <robin.peter.vkm@gmail.com>
2021-03-09 16:12:16 -0400

    Step two -- FilterProvider

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/filter/FilteringController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/filter/FilteringController.java
@@ -7,12 +7,17 @@ import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+
 @RestController
 public class FilteringController {
 	//field1,field2
 	@GetMapping("/filtering")
 	public SomeBean retriveSomeBean() {
 		SomeBean someBean = new SomeBean("value1","value2","value3");
+		
+		FilterProvider filterProvider = new  SimpleFilterProvider().addFilter("SomeBean", filter)
 		
 		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean);
 		
