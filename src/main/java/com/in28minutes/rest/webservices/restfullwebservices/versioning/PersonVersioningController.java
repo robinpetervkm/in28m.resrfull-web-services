b36f3102b4c0d6650f0b200f6c7b598a6859dfdc
Robin Peter <robin.peter.vkm@gmail.com>
2021-03-09 17:32:05 -0400

    Versioning comparison

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/versioning/PersonVersioningController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/versioning/PersonVersioningController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonVersioningController {
 	
+	
+	//URI Versioning---Twitter
 	@GetMapping("v1/person")
 	public PersonV1 personV1() {
 		return new PersonV1("Bob Charlie");
@@ -15,6 +17,8 @@ public class PersonVersioningController {
 	public PersonV2 personV2() {
 		return new PersonV2(new Name("Bob", "Charlie"));
 	}
+	
+	//Reqest param Versioning---- AMAZON
 	@GetMapping(value="person/param",params = "version=1")
 	public PersonV1 paramV1() {
 		return new PersonV1("Bob Charlie");
@@ -24,6 +28,9 @@ public class PersonVersioningController {
 	public PersonV2 paramV2() {
 		return new PersonV2(new Name("Bob", "Charlie"));
 	}
+	
+	//Custom Versioning----Microsoft
+	
 	@GetMapping(value="person/header",headers = "X-API-VERSION=1")
 	public PersonV1 headerV1() {
 		return new PersonV1("Bob Charlie");
@@ -33,6 +40,8 @@ public class PersonVersioningController {
 	public PersonV2  headerV2() {
 		return new PersonV2(new Name("Bob", "Charlie"));
 	}
+	
+	// Media type - Content negotiation, accept Header versioning--- GitHub
 	@GetMapping(value="person/produces",produces = "application/vnd.company.app-v1+json")
 	public PersonV1 producesV1() {
 		return new PersonV1("Bob Charlie");
@@ -43,4 +52,17 @@ public class PersonVersioningController {
 		return new PersonV2(new Name("Bob", "Charlie"));
 	}
 
+	/*Factors
+		- URI Pollution--- URI and Custom
+		- MisUse of URI Header
+		- Caching-- difficult in media type and Custom
+		- Can we execute the request on the browser--- difficult in media type and Custom
+		- API Documentation--difficult in media type and Custom
+	- no perfect solution 
+	
+	Filalize the versioning starting of the project
+	
+	*/
+	
+	
 }
