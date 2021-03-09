107310910bfe4229d51321f9c9960e5269a1e38a
Robin Peter <robin.peter.vkm@gmail.com>
2021-03-09 16:09:45 -0400

    step one -- MappingJacksonValue

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/filter/FilteringController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/filter/FilteringController.java
@@ -3,15 +3,24 @@ package com.in28minutes.rest.webservices.restfullwebservices.filter;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FilteringController {
+	//field1,field2
 	@GetMapping("/filtering")
 	public SomeBean retriveSomeBean() {
-		return new SomeBean("value1","value2","value3");
+		SomeBean someBean = new SomeBean("value1","value2","value3");
+		
+		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean);
+		
+		return someBean;
+		
+		
 	}
+	//field2,filed3
 	@GetMapping("/filtering-list")
 	public List<SomeBean> retriveListOfSomeBean() {
 		return Arrays.asList(new SomeBean("value1","value2","value3"),
